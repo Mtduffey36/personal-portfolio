48b123af62a7846b1435059443a87db113a5b56c
Mtduffey36 <michael.t.duffey@gmail.com>
2024-09-06 20:05:31 -0500

    change vite config

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,5 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   root: '.', // Ensure this points to the root directory where index.html is located
-  build: {
-    rollupOptions: {
-      input: 'index.html', // Ensure this points to the correct entry file
-    },
-  },
 });
 

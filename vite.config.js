6624c8f3a999b8219ac3a871a94e979cf39afb2f
Mtduffey36 <michael.t.duffey@gmail.com>
2024-09-06 20:08:40 -0500

    change vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,16 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: '.', // Ensure this points to the root directory where index.html is located
+  root: '.',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`
+      }
+    }
+  } // Ensure this points to the root directory where index.html is located
 });
 

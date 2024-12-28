fd1bde0f77bb25bb15b15bdd4368918b8c82ce1b
Mtduffey36 <michael.t.duffey@gmail.com>
2024-12-28 11:25:26 -0600

    added line in aboutMe

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -5,7 +5,7 @@ const AboutMe = () => {
         <section className="p-4">
             <img src="./assets/images/pitts-125.jpg" alt="Michael Duffey" className="w-32 h-32 rounded-full mx-auto"/>
             <p className="mt-4 text-center">
-                Hi, I'm a passionate web developer with expertise in the MERN stack. I love creating responsive and dynamic web applications.
+            Hi, I'm a passionate web developer with expertise in the MERN stack. I love creating responsive and dynamic web applications. And I love cookies!
             </p>
         </section>
     );
